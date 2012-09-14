ebe56c5a1017a940897a35974cbf0cea87f1babd
Angeldar <angeldaren@gmail.com>
2012-09-14 22:52:31 +0300

    Added post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+posts = Post.create([{ title: "Hello world!", content: "This is my first post" },
+					{ title: "Second post", content: "And after first, here is second post."}])
